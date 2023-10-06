9fbaf327344923a3832190f7a343a54876836df4
GabiSBD <gabrielcruz1292@gmail.com>
2023-10-06 20:13:45 +0200

    hotfix sobre la anulacion de codigo sobrante

--- src/view/js/index.js
+++ src/view/js/index.js
@@ -2,7 +2,6 @@ $(function(){
       
         $("#typeWriter").click(toProcesador);
         $("#logo").click(toIndex);
-        $("#calculator").click(toCalculator);
         $("#galery").click(toGalery);
         $("#adminArea").click(toAdminArea);
         
@@ -10,7 +9,6 @@ $(function(){
     
   });
   function toProcesador(){ window.location = "procesador.php"; }
-  function toCalculator(){ window.location = "calculadora.php"; }
   function toIndex(){ window.location = "index.php"; }
   function toGalery(){ window.location = "galery.php"; }
   function toAdminArea(){ window.location = "adminArea.php"; }
